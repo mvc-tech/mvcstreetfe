af8cd64d0183755ba3bd0c4aa6c83d80414483ad
l.doneddu <l.doneddu.mvc@gmail.com>
2023-12-21 15:06:14 +0100

    tabella js

--- program.php
+++ program.php
@@ -5,51 +5,64 @@ include_once 'res/conn.php';
 ?>
 
 <div class="container-fluid">
-<div class="container">
-  <h2>Rilevamenti</h2>            
-  <table class="table table-striped tabella-rilievi">
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>TARGA</th>
-        <th>Data</th>
-        <th>ID Macchina</th>
-        <th>Zona</th>
-        <th>Eccezione</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php foreach(fetchRilevamenti($conn) as $res):?>
-      <tr>
-        <td><?=$res['id']?></td>
-        <td><?=$res['targa']?></td>
-        <td><?=$res['dataora']?></td>
-        <td><?=$res['id_macchina']?></td>
-        <td><?=$res['zona']?></td>
-        <td><?php foreach(fetchEccezioni($conn) as $ecc):?>
-            <span class="bottone"><a onclick="funzione()"><img class="iconcina" src="<?php 
-                switch($ecc){
-                    case 'residente':
-                        echo 'assets/resident.png';
-                        break;
-                    case 'disabile':
-                        echo 'assets/disabled.png';
-                        break;
-                    case 'ibrida':
-                        echo 'assets/hybrid.png';
-                        break;
-                    case 'abbonato':
-                        echo 'assets/subscription.png';
-                        break;
-                    case 'biglietto':
-                        echo 'assets/ticket.png';
-                }
-                ?>
-            "/></a></span>
-        <?php endforeach?></td>
-      </tr>
-      <?php endforeach;?>
-    </tbody>
-  </table>
+    <div class="container">
+        <div class="big-spacer"></div>
+    <h2>Rilevamenti</h2>            
+    <table class="table table-striped tabella-rilievi">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>TARGA</th>
+            <th>Data</th>
+            <th>ID Macchina</th>
+            <th>Zona</th>
+            <th>Eccezioni</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach(fetchRilevamenti($conn) as $res):?>
+        <tr id="<?=$res['id']?>">
+            <td><?=$res['id']?></td>
+            <td><?=$res['targa']?></td>
+            <td><?=$res['dataora']?></td>
+            <td><?=$res['id_macchina']?></td>
+            <td><?=$res['zona']?></td>
+            <td><?php foreach(fetchEccezioni($conn) as $ecc):?>
+                <span class="nopadding"><a onclick="hideElement(<?=$res['id']?>)"><img <?php 
+                    switch($ecc){
+                        case 'residente':
+                            echo 'class="bottone iconcina" id="residente" src="assets/resident.png"';
+                            break;
+                        case 'disabile':
+                            echo 'class="bottone iconcina" id="disabile" src="assets/disabled.png"';
+                            break;
+                        case 'ibrida':
+                            echo 'class="bottone iconcina" id="ibrida" src="assets/hybrid.png"';
+                            break;
+                        case 'abbonato':
+                            echo 'class="bottone iconcina" id="abbonato" src="assets/subscription.png"';
+                            break;
+                        case 'biglietto':
+                            echo 'class="bottone iconcina" id="biglietto" src="assets/ticket.png"';
+                    }
+                    ?>
+                /></a></span>
+            <?php endforeach?></td>
+        </tr>
+        <?php endforeach;?>
+        </tbody>
+    </table>
+    </div>
 </div>
-</div>
+
+<script>
+
+    function hideElement(element){
+        var riga = document.getElementById(element);
+        riga.style.display = 'none';
+        console.log(element);
+    }
+
+    
+
+</script>
